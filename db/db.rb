df745ec7d3fd48dd5657f7c4758944077475fae2
oscar <opawong@yahoo.com>
2022-02-22 15:26:18 +1100

    create database

--- db/db.rb
+++ db/db.rb
@@ -1,7 +1,7 @@
 require 'pg'
 
 def run_sql(sql, sql_params = [])
-    db = PG.connect(ENV['DATABASE_URL'] || {dbname: 'next_timesheet'})
+    db = PG.connect(ENV['DATABASE_URL'] || {dbname: 'next_timesheet_db'})
     result = db.exec_params(sql, sql_params)
     db.close
     result
---
+++ db/schema.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE next_timesheet_db;
